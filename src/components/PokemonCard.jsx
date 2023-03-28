0ec7a43203cda503adbf1f063efb0316be680916
tony3717 <gentytony@gmail.com>
2023-03-28 12:36:16 +0200

    Pokemon

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+
+function PokemonCard() {
+return <figure>
+<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+
+alt="bulbasaur" />
+
+<figcaption> bulbasaur</figcaption>
+</figure>
+
+
+}
+export default PokemonCard
